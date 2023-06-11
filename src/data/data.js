b39863bbe34af7d7fd6178904cfeb1fe145d78e0
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-11 06:08:57 +0200

    first upload

---
+++ src/data/data.js
@@ -0,0 +1,106 @@
+export const LARGE_IMAGES = [
+  {
+    id: 1,
+    src: "/images/placeholder-pdf.webp",
+    alt: "Placeholder image",
+    comment: (
+      <p style={{ color: "white", lineHeight: 1 }}>
+        Click{" "}
+        <a
+          href="/pdf/placeholder-pdf.pdf"
+          style={{
+            color: "#ADD8E6",
+            lineHeight: 1,
+            fontSize: "xx-large",
+            padding: 0,
+          }}
+        >
+          download
+        </a>{" "}
+        to read the entire PDF on how you can conserve water usage.
+      </p>
+    ),
+  },
+  {
+    id: 2,
+    src: "/images/placeholder-pdf.webp",
+    alt: "Placeholder image",
+    comment: (
+      <p
+        style={{
+          color: "white",
+          lineHeight: 1,
+        }}
+      >
+        Go to&nbsp;
+        <a
+          href="https://hood.de"
+          style={{ textDecoration: "none", color: "yellow" }}
+        >
+          hood.de
+        </a>
+        &nbsp; for stuff you generally cant find on ebay!
+      </p>
+    ),
+  },
+  {
+    id: 3,
+    src: "/images/placeholder-pdf.webp",
+    alt: "Placeholder image",
+    comment: (
+      <p
+        style={{
+          color: "white",
+          lineHeight: 1,
+        }}
+      >
+        "This is an example of a user fillable PDF form. Normally PDF is used as
+        a final publishing format. However PDF has an option to be used as an
+        entry form that can be edited and saved by the user." &nbsp;
+        <a href="/images/pdf4.pdf" style={{ color: "blue", lineHeight: 1 }}>
+          Click here
+        </a>{" "}
+        to download PDF
+      </p>
+    ),
+  },
+  {
+    id: 4,
+    src: "/images/placeholder-pdf.webp",
+    alt: "Placeholder image",
+    comment: (
+      <a
+        href="https://beaker.com"
+        style={{ color: "white", lineHeight: 10, padding: 20 }}
+      >
+        Beaker, cant remember what its actually called
+      </a>
+    ),
+  },
+  {
+    id: 5,
+    src: "/images/placeholder-pdf.webp",
+    alt: "Placeholder image",
+    comment: (
+      <a
+        href="/pdf/placeholder-pdf.pdf"
+        style={{ color: "white", lineHeight: 10, padding: 20 }}
+      >
+        The entire Orchard Beach Community Group - Board Meeting Minutes
+      </a>
+    ),
+  },
+  {
+    id: 6,
+    src: "/images/placeholder-pdf.webp",
+    alt: "Placeholder image",
+    comment: (
+      <a
+        href="/pdf/placeholder-pdf.pdf"
+        style={{ color: "white", lineHeight: 10, padding: 20 }}
+      >
+        600-Series Harmony Remote - User Manual
+      </a>
+    ),
+  },
+];
